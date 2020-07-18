578ae8cdd6bfb96af96abe1bad465467461742a6
mavissauro <mvinicius.ribeiro68@gmail.com>
2020-07-17 23:36:59 -0300

    Adding performance optimization and handling errors on the project

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-import { SpinnerOverlay, SpinnerContainer } from "./with-spinner.styles";
+import Spinner from "../spinner/spinner.component";
 
 const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
-  return isLoading ? (
-    <SpinnerOverlay>
-      <SpinnerContainer />
-    </SpinnerOverlay>
-  ) : (
-    <WrappedComponent {...otherProps} />
-  );
+  return isLoading ? <Spinner /> : <WrappedComponent {...otherProps} />;
 };
 
 export default WithSpinner;
